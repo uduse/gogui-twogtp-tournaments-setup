c27be87acf89ce054028e43e692c47dff6cb7260
uduse <i@uduse.com>
2020-05-27 13:10:40 -0600

    Update.

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 : ${WHITE:=./orig_katago.sh}
 : ${BLACK:=./mmcts_katago.sh}
 : ${NUM_GAMES:=10}
+: ${NUM_THREADS:=10}
 
 NOW=$(date +"%T")
-gogui-twogtp -black $BLACK -white $WHITE -verbose -threads 10 -alternate -games ${NUM_GAMES} -auto -sgffile result-${NOW}
+gogui-twogtp -black $BLACK -white $WHITE -verbose -threads $NUM_THREADS -alternate -games ${NUM_GAMES} -auto -sgffile result-${NOW} 2>&1 | gnomon
 gogui-twogtp -analyze result-${NOW}.dat
