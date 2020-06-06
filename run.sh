026410a9ecbef072d4297589b39185df871caa29
uduse <i@uduse.com>
2020-06-05 22:15:35 -0600

    Update.

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 : ${BLACK:=./mmcts_katago.sh}
 : ${NUM_GAMES:=10}
 : ${NUM_THREADS:=10}
+: ${BOARD_SIZE:=9}
 
 NOW=$(date +"%T")
 echo 'NUM_THREADS:' $NUM_THREADS
 echo 'NUM_GAMES:' $NUM_GAMES
-gogui-twogtp -black $BLACK -white $WHITE -verbose -threads $NUM_THREADS -alternate -games ${NUM_GAMES} -auto -sgffile result-${NOW} 2>&1 | gnomon
+gogui-twogtp -black $BLACK -white $WHITE -size $BOARD_SIZE -verbose -threads $NUM_THREADS -alternate -games ${NUM_GAMES} -auto -sgffile result-${NOW} 2>&1 | gnomon
 gogui-twogtp -analyze result-${NOW}.dat
