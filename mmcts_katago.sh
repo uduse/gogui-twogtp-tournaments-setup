0b862eaa48663e688104ef05cbb1577693df87a2
uduse <i@uduse.com>
2020-06-11 13:28:43 -0600

    Update.

--- mmcts_katago.sh
+++ mmcts_katago.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-valgrind ../KataGo/cpp/katago gtp -model model.bin.gz -config <(cat base.cfg mmcts_extra.cfg) -override-config logFile="mmcts.log"
+LOG_ID=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+../KataGo/cpp/katago gtp -model model.bin.gz -config <(cat base.cfg mmcts_extra.cfg) -override-config logFile="mmcts_${LOG_ID}.log"
