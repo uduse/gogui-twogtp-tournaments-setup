a65058c5a8ca699f749193ab7b0a1bfe23bed6a9
uduse <i@uduse.com>
2020-06-13 17:51:03 -0600

    Update.

--- gomill.sh
+++ gomill.sh
@@ -6,4 +6,6 @@
 echo 'NUM_GAMES:' $NUM_GAMES
 echo 'NUM_THREADS:' $NUM_THREADS
 
-ringmaster <(cat gomill.ctl | sed "s/number_of_games=10/number_of_games=${NUM_GAMES}/g" ) run -j ${NUM_THREADS}
+cat gomill.ctl | sed "s/number_of_games=10/number_of_games=${NUM_GAMES}/g" | sed "s/board_size = 9/board_size = ${BOARD_SIZE}/g" > .tmp.ctl
+ringmaster .tmp.ctl run -j ${NUM_THREADS}
+rm -f .tmp.ctl
