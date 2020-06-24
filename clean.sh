f3e22fbf3ab31ad31bf1a2bc326e3636df33e754
Zeyi Wang <uduse@cedar5.cedar.computecanada.ca>
2020-06-23 17:14:58 -0700

    Update.

--- clean.sh
+++ clean.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-rm -f result-*
 rm -f *.log
 ringmaster gomill.ctl reset
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-NUM_THREADS=1 NUM_GAMES=1 ./run.sh
+NUM_THREADS=1 NUM_GAMES=1 ./gomill.sh
